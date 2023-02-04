523c011681c3eda6c1b57ab9868d5d9c4c2bd2cd
Senem Ozcetin <snmzctn91@gmail.com>
2023-02-04 21:21:17 +0300

    04/02/23

--- src/test/java/techproed/tests/Day19_TestNGAnnotations.java
+++ src/test/java/techproed/tests/Day19_TestNGAnnotations.java
@@ -62,7 +62,9 @@ public class Day19_TestNGAnnotations {
     public void afterMethod(){
         System.out.println("After Method");
     }
-    @Test(priority = 1)
+
+
+    @Test(priority = 1,groups = "regression-tests")
     public void test7(){
         System.out.println("Test 7");
     }
@@ -70,7 +72,7 @@ public class Day19_TestNGAnnotations {
     public void test2(){
         System.out.println("Test 2");
     }
-    @Test (enabled = false)
+    @Test (enabled = false,groups = "regression-tests")
     public void test3(){
         System.out.println("Test 3");
     }
--- src/test/java/techproed/tests/Day21_TechproLoginTest.java
+++ src/test/java/techproed/tests/Day21_TechproLoginTest.java
@@ -1,14 +1,24 @@
 package techproed.tests;
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import techproed.pages.TechproHomePage;
 import techproed.pages.TechproLoginPage;
 import techproed.utilities.Driver;
 public class Day21_TechproLoginTest {
-    @Test
-    public void loginTest() {
+    @Test(groups = "regression-tests")
+    public void loginTest(){
         Driver.getDriver().get("https://testcenter.techproeducation.com/index.php?page=form-authentication");
         TechproLoginPage techproLoginPage = new TechproLoginPage();
+        TechproHomePage techproHomePage = new TechproHomePage();
         techproLoginPage.username.sendKeys("techproed");
         techproLoginPage.password.sendKeys("SuperSecretPassword");
         techproLoginPage.submitButton.click();
+        // Assertion
+        // Login yapildi, Driver su anda HomePage de
+        Assert.assertTrue(techproHomePage.homeHeader.isDisplayed());
+        // Sayfadan cikis yap ve cikis yapildigini test et
+        techproHomePage.homeLogoutButton.click();
+        // Cikis olduguna dair assertion yap
+        Assert.assertTrue(techproLoginPage.submitButton.isDisplayed());
     }
 }
